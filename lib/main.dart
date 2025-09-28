db283f5321996b3fc6d7e977a330e67551729fff
DantalianFree <mdrojo.chmsu@gmail.com>
2025-09-29 00:43:12 +0800

    Drastic Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'main_layout_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const MainLayoutScreen(),
     );
   }
 }
